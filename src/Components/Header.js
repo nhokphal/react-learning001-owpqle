86597bf5726ba3d76572961cf9e7bdc00e24ebdc
nhokphal <phalnhok@gmail.com>
2020-09-10 16:50:35 +0700

    1 file modified

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,11 +5,11 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-      return <div className="Header" />;
-      <div>
-      <h1>Router</h1>
-      </div>
-                
+              return <div className="Header" />;
+              <div>
+                  <h1>Header</h1>
+              </div>
                 }
       }
+
 export default Header;
